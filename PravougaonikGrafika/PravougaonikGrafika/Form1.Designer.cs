0604d9772443fae476386ef326a68a8a10d28bd1
mikikupus <mihajlo.randjelovic04@outlook.com>
2022-12-21 23:37:47 +0100

    dsa

---
+++ PravougaonikGrafika/PravougaonikGrafika/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PravougaonikGrafika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtelement = new System.Windows.Forms.TextBox();
+            this.btdodaj = new System.Windows.Forms.Button();
+            this.btukloni = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtelement
+            // 
+            this.txtelement.Location = new System.Drawing.Point(34, 28);
+            this.txtelement.Name = "txtelement";
+            this.txtelement.Size = new System.Drawing.Size(100, 20);
+            this.txtelement.TabIndex = 0;
+            // 
+            // btdodaj
+            // 
+            this.btdodaj.Location = new System.Drawing.Point(34, 54);
+            this.btdodaj.Name = "btdodaj";
+            this.btdodaj.Size = new System.Drawing.Size(75, 23);
+            this.btdodaj.TabIndex = 1;
+            this.btdodaj.Text = "Dodaj";
+            this.btdodaj.UseVisualStyleBackColor = true;
+            this.btdodaj.Click += new System.EventHandler(this.btdodaj_Click);
+            // 
+            // btukloni
+            // 
+            this.btukloni.Location = new System.Drawing.Point(34, 83);
+            this.btukloni.Name = "btukloni";
+            this.btukloni.Size = new System.Drawing.Size(75, 23);
+            this.btukloni.TabIndex = 2;
+            this.btukloni.Text = "Ukloni";
+            this.btukloni.UseVisualStyleBackColor = true;
+            this.btukloni.Click += new System.EventHandler(this.btukloni_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btukloni);
+            this.Controls.Add(this.btdodaj);
+            this.Controls.Add(this.txtelement);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtelement;
+        private System.Windows.Forms.Button btdodaj;
+        private System.Windows.Forms.Button btukloni;
+    }
+}
+
---
+++ PravougaonikGrafika/PravougaonikGrafika/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*genericki red sadrzi stringove, omoguciti dodavanje i uklanjanje iz reda. Rez moze imati max 5 elemenata
+ red na formi prikazati graficki kao pravougaonik koji:
+-kada je prazan je obojen belom bojom
+-ako je popunjen 20% taj element je prikazan zutom bojom
+-ako ima 2 elementa oni su prikazani zelenom bojom
+-za 3 elementa plava boja
+-za 4 crvena
+-ako je red potpuno popunjen crnom bojom*/
+namespace PravougaonikGrafika
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> red;
+        Graphics g;
+        Brush b;
+        int x = 300;
+        int y = 30;
+        int height = 50;
+        int width = 100;
+        bool nacrtano = false;
+        int j = 4;
+        Color[] boje = new Color[] { Color.White, Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Black };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            red = new Queue<string>();
+            g = CreateGraphics();
+
+        }
+
+        private void btdodaj_Click(object sender, EventArgs e)
+        {
+            if (txtelement.Text != "" && red.Count < 5)
+            {
+                red.Enqueue(txtelement.Text);
+                DodajPravougaonik();
+                txtelement.Text = "";
+            }
+        }
+        private void btukloni_Click(object sender, EventArgs e)
+        {
+            if (red.Count > 0) 
+            { 
+                UkloniPravougaonik(); 
+            }
+        }
+        private void DodajPravougaonik()
+        {
+            if(red.Count <=5)
+            {
+                b = new SolidBrush(boje[red.Count]);
+                if (red.Count < 5 && red.Count >= 0)
+                {
+                    g.FillRectangle(b, x, y + ((j-1) * height), 100, 50);
+                    j--;
+                }
+                else
+                {
+                    g.FillRectangle(b, x, y, 100, 200);
+                }
+            }
+        }
+        private void UkloniPravougaonik()
+        {
+            if(red.Count > 0)
+            {
+                if (red.Count == 5)
+                {
+                    int k = 4;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        b = new SolidBrush(boje[k--]);
+                        g.FillRectangle(b, x, y + (i * height), 100, 50);
+                    }
+                }
+                else
+                {
+                    b = new SolidBrush(boje[0]);
+                    g.FillRectangle(b, x, y + (j++ * height), 100, 50);
+                }
+                red.Dequeue();
+            }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if(!nacrtano)
+            {
+                b = new SolidBrush(boje[0]);
+                for (int i = 0; i < 4; i++)
+                {
+                    g.FillRectangle(b, x, y + ((i) * height), 100, 50);
+                }
+                nacrtano = true;
+            }
+           
+        }
+    }
+}
